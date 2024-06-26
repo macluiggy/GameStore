e647d8f770a510cf285d27b646b75f6170c78375
macluiggy <luiggy.macias@imagineapps.co>
2024-06-26 17:17:14 -0500

    chore: Add SQLite database configuration to Program.cs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,10 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
